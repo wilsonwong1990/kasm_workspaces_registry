53465c78b96ff96cc0e012d7442f9849527ec199
Chris Hunt <chris.hunt@kasmweb.com>
2024-08-14 13:45:49 +0100

    Change workspace count

--- site/pages/index.js
+++ site/pages/index.js
@@ -71,7 +71,7 @@ export default function Home({ searchText }) {
         <h1 className='flex flex-wrap-reverse uppercase tracking-widest justify-center mb-10 gap-5'>
         <span className='flex items-center text-lg bg-slate-100/90 rounded overflow-hidden shadow'>
             <span className='flex px-3 text-xs opacity-100'>Workspaces</span>
-            <span className='text-white p-3 py-1 flex bg-[#2980b9]'>{workspaces && workspaces.workspacecount}</span>
+            <span className='text-white p-3 py-1 flex bg-[#2980b9]'>{filteredworkspaces && filteredworkspaces.length}</span>
           </span>
           <span className='flex items-center text-lg bg-slate-100/90 rounded overflow-hidden shadow'>
             <span className='flex px-3 text-xs opacity-100'>Kasm Version</span>
