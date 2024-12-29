49cbb051b85e884a750323b4544a2750be64a8ab
Wilson Wong <wilsonwong1990@gmail.com>
2024-12-29 10:23:11 -0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm_workspaces_registry/1.0',
+  basePath: '/kasm_workspaces_registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
