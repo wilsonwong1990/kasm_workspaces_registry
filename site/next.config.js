94b161e4da6c4d0aa06581c787e989ee7a7f8b36
Wilson Wong <wilsonwong1990@gmail.com>
2024-12-29 10:20:16 -0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'wwong',
+    description: 'Some containers yo.',
+    icon: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg',
+    listUrl: 'https://blog.wwong.me/kasm_workspaces_registry/',
+    contactUrl: 'https://github.com/wilsonwong1990',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm_workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
